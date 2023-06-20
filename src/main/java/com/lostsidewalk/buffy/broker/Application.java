5b727c3404113d9b4227077f25ef72d5d319670e
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2023-06-19 21:20:21 -0500

    Added Spring material, basic controller buid-out

---
+++ src/main/java/com/lostsidewalk/buffy/broker/Application.java
@@ -0,0 +1,29 @@
+package com.lostsidewalk.buffy.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableScheduling
+@EnableConfigurationProperties
+//@PropertySource("classpath:secret.properties")
+@ComponentScan({"com.lostsidewalk.buffy", "com.lostsidewalk.buffy.broker"})
+@Configuration
+public class Application {
+
+    public static void main(String[] args) {
+        //
+        // set global timeouts and startup/context config
+        //
+        System.setProperty("sun.net.client.defaultConnectTimeout", "2000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "4000");
+        System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+        SpringApplication.run(Application.class, args);
+    }
+}
