e2a1224812f6b4bfcf52b7ec7a7d8e1e0d92e66a
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2023-08-09 13:21:25 -0500

    Spring web security updates

--- src/main/java/com/lostsidewalk/buffy/broker/security/WebSecurityConfig.java
+++ src/main/java/com/lostsidewalk/buffy/broker/security/WebSecurityConfig.java
@@ -1,16 +1,20 @@
 package com.lostsidewalk.buffy.broker.security;
 
 import com.lostsidewalk.buffy.broker.auth.AuthTokenFilter;
+import com.lostsidewalk.buffy.broker.user.LocalUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -21,37 +25,51 @@ import static org.springframework.http.HttpMethod.OPTIONS;
 @Configuration
 @EnableWebSecurity
 @EnableWebSocketSecurity
-@EnableMethodSecurity(
-		securedEnabled = true,
-		jsr250Enabled = true)
+@EnableMethodSecurity
 class WebSecurityConfig {
 
 	@Autowired
+	LocalUserService userDetailsService;
+
+	@Autowired
 	AuthTokenFilter authTokenFilter;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder);
+
+		return authProvider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+		return authConfiguration.getAuthenticationManager();
+	}
 
 	@Value("${newsgears.originUrl}")
 	String feedGearsOriginUrl;
 
 	@Bean
-	public AuthenticationManager authenticationManager() {
-		return authentication -> new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials());
-	}
-
-	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-				.httpBasic().disable()
-				.cors().configurationSource(request -> {
+				.httpBasic(AbstractHttpConfigurer::disable)
+				.cors(c -> c.configurationSource(request -> {
 					CorsConfiguration configuration = new CorsConfiguration();
 					configuration.setAllowedOriginPatterns(singletonList(this.feedGearsOriginUrl));
 					configuration.setAllowedMethods(singletonList("*"));
 					configuration.setAllowedHeaders(singletonList("*"));
 					configuration.setAllowCredentials(true);
 					return configuration;
-				}).and()
-				.csrf().and()
-				.headers()
-				.and().authorizeHttpRequests()
+				}))
+				.csrf(c -> c.configure(http))
+				.headers(h -> h.configure(http))
+				.authorizeHttpRequests(a -> a
 					// permit pre-auth
 					.requestMatchers("/").permitAll() // index
 					.requestMatchers("/secured/room").permitAll() // bypass AuthTokenFilter for browser requests
@@ -59,7 +77,7 @@ class WebSecurityConfig {
 					// permit options calls
 					.requestMatchers(OPTIONS, "/**").permitAll() // OPTIONS calls are validated downstream by checking for the presence of required headers
 					// (all others require authentication)
-					.anyRequest().authenticated();
+					.anyRequest().authenticated());
 		http.addFilterBefore(this.authTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
